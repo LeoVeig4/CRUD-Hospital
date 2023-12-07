1c1a1f1cec94abb490b7de77830f64af7e1ee9f6
LeoVeig4 <leoveiga2002@gmail.com>
2023-12-07 03:42:58 -0300

    Update .gitignore and docker-compose.yml SUCESSO

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 # app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 # app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
-app.config['MYSQL_HOST'] = 'crud-hospital-db-1'
+app.config['MYSQL_HOST'] = 'db'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'flask5k'
